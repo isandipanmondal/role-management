86bac71461548e1624bd8476f69ae66dcc014160
Sandipan Mondal <i.sandipanmondal@gmail.com>
2019-03-23 09:53:38 +0530

    Before admin menu section

--- wp-content/plugins/wrm/wrm.php
+++ wp-content/plugins/wrm/wrm.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /**
  * @package wrm
@@ -15,66 +15,91 @@ Text Domain: wrm
 
 if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
+
 if( ! class_exists('WRM') ) :
 
-/**
-* 
-*/
-class WRM
-{
     /**
-     * WRM constructor.
+     *
      */
-	function __construct()
-	{
-		
-	}
+    class WRM
+    {
+        /**
+         * WRM constructor.
+         */
+        function __construct()
+        {
+            add_action( 'admin_notices', array($this, 'active'));
 
-    /**
-     * @var string
-     */
-	var $version = '1.0';
+            /*
+             *
+             */
 
-    /**
-     * Initialize basic vars
-     */
-	public function initialize()
-	{
-		// vars
-		$version = $this->version;
-		$basename = plugin_basename( __FILE__ );
-		$path = plugin_dir_path( __FILE__ );
-		$url = plugin_dir_url( __FILE__ );
-		$slug = dirname($basename);
+            /** Step 2 (from text above). */
+            add_action( 'admin_menu', array($this, 'wpdocs_register_my_custom_menu_page') );
 
-	}
 
-    /**
-     * Create WRM Table
-     * @throws Exception
-     */
-	function createWRMTable() 
-	{
-		try {
-            global $wpdb;
-            $charset_collate = $wpdb->get_charset_collate();
-            $table_name = $wpdb->prefix . "wrm";
-            $this->wrmQuery($table_name, $charset_collate);
-
-        } catch (Exception $e) {
-            $this->wrmErrorHandle($e);
         }
-	}
 
-    /**
-     * Query for WRM table
-     * @param $table_name
-     * @throws Exception
-     */
-	function wrmQuery($table_name, $charset_collate)
-	{
-		try {	
-			$sql = "CREATE TABLE $table_name (
+        /**
+         * @var string
+         */
+        var $version = '1.0';
+
+        /**
+         * Initialize basic vars
+         */
+        public function initialize()
+        {
+            // vars
+            $version = $this->version;
+            $basename = plugin_basename( __FILE__ );
+            $path = plugin_dir_path( __FILE__ );
+            $url = plugin_dir_url( __FILE__ );
+            $slug = dirname($basename);
+
+        }
+
+        /**
+         * Create WRM Table
+         * @throws Exception
+         */
+        function createWRMTable()
+        {
+            try {
+
+                global $wpdb;
+                $table_name = $wpdb->prefix . "wrm";
+
+                if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name ) {
+
+                    $charset_collate = $wpdb->get_charset_collate();
+                    $this->wrmQuery($table_name, $charset_collate);
+
+                } else {
+
+                    //echo "The table name is exists!";
+                    ?>
+                    <div class="notice notice-error is-dismissible">
+                        <p><?php _e( 'WRM table already exists!'); ?></p>
+                    </div>
+                    <?php
+
+                }
+
+            } catch (Exception $e) {
+                $this->wrmErrorHandle($e);
+            }
+        }
+
+        /**
+         * Query for WRM table
+         * @param $table_name
+         * @throws Exception
+         */
+        function wrmQuery($table_name, $charset_collate)
+        {
+            try {
+                $sql = "CREATE TABLE $table_name (
 			  id mediumint(9) NOT NULL AUTO_INCREMENT,
 			  time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
 			  name tinytext NOT NULL,
@@ -83,31 +108,121 @@ class WRM
 			  PRIMARY KEY  (id)
 			) $charset_collate;";
 
-			require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-			dbDelta( $sql );
-		} catch (Exception $e) {
-			$this->wrmErrorHandle($e);
-		}
-	}
+                require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+                dbDelta( $sql );
+            } catch (Exception $e) {
+                $this->wrmErrorHandle($e);
+            }
+        }
 
-    /**
-     * Error handle
-     * @param $e
-     * @throws Exception
-     */
-	function wrmErrorHandle($e)
-	{
-		throw new Exception($e->getMessage());
-	}
+        /**
+         * Error handle
+         * @param $e
+         * @throws Exception
+         */
+        function wrmErrorHandle($e)
+        {
+            throw new Exception($e->getMessage());
+        }
 
-    /**
-     * Activation and Deactivation Hook
-     * Trigger Create WRM Table Function
-     * Check if table already exists
-     */
 
-}
 
-$wrm = new WRM;
+        /**
+         * Remove database table
+         */
+
+        function removeWRMtable ()
+        {
+            /*
+             * Making WPDB as global
+             * to access database information.
+             */
+            global $wpdb;
+
+            /*
+             * @var $table_name
+             * name of table to be dropped
+             * prefixed with $wpdb->prefix from the database
+             */
+            $table_name = $wpdb->prefix . 'wrm';
+
+            // drop the table from the database.
+            $wpdb->query( "DROP TABLE IF EXISTS $table_name" );
+        }
+
+
+
+        /**
+         * New admin menu
+         */
+        /**
+         * Register a custom menu page.
+         */
+        function wpdocs_register_my_custom_menu_page(){
+            add_menu_page(
+                __( 'Custom Menu Title', 'textdomain' ),
+                'custom menu',
+                'manage_options',
+                'custompage',
+                'my_custom_menu_page',
+                plugins_url( 'myplugin/images/icon.png' ),
+                6
+            );
+        }
+
+
+        /**
+         * Display a custom menu page
+         */
+        function my_custom_menu_page(){
+            esc_html_e( 'Admin Page Test', 'textdomain' );
+        }
+
+
+
+        /**
+         * Activation hook
+         */
+
+        function active()
+        {
+            $this->createWRMTable();
+            $this->wpdocs_register_my_custom_menu_page();
+
+            flush_rewrite_rules();
+        }
+
+        /**
+         * Deactivation hook
+         */
+
+        function deactivate()
+        {
+           // $this->removeWRMtable();
+            flush_rewrite_rules();
+        }
+
+        /**
+         * Activation and Deactivation Hook / Done
+         * Trigger Create WRM Table Function / Done
+         * Check if table already exists / Done
+         * Add main and sub menu for wrm
+         * Options are general(3 sections admin, author, editor), settings
+         */
+
+    }
+
+    $wrm = new WRM;
 
 endif;
+
+/**
+ * This is activation hook
+ */
+
+register_activation_hook( __FILE__, array( $wrm, 'active' ) );
+
+/**
+ * This is deactivation hook
+ */
+register_deactivation_hook( __FILE__, array( $wrm, 'deactivate') );
